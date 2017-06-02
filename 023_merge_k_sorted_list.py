b34c3ee1099b587a106898d33d3fdb4aa3c7fd92
silentmaker <demonmaxc@gmail.com>
2017-06-02 19:04:17 +0800

    merge k sorted list solved, mostly by python itself

--- 023_merge_k_sorted_list.py
+++ 023_merge_k_sorted_list.py
@@ -4,10 +4,63 @@
 #         self.val = x
 #         self.next = None
 
+# class Solution(object):
+#     def mergeKLists(self, lists):
+#         # lists: List[ListNode]
+#         # example testing: [[1,2,3],[3,3,6]] => [1,2,3,3,3,6]
+#         result = []
+#         minium = 'MAX'
+#         min_index = 'M_INDEX'
+#         count = 0
+#         i = 0
+#         k = len(lists)
+#         if k == 0:
+#             return lists
+#         else:
+#             while True:
+#                 if lists[i] == None:
+#                     count = count + 1
+#                 elif lists[i].val < minium:
+#                     minium = lists[i].val
+#                     min_index = i           
+#                 if count == k:
+#                     break
+#                 if i + 1 == k:
+#                     i = 0
+#                     if min_index != 'M_INDEX':
+#                         lists[min_index] = lists[min_index].next
+#                     result.append(minium)
+#                     minium = 'MAX'
+#                     min_index = 'M_INDEX'
+#                     count = 0
+#                 else:
+#                     i = i + 1
+#         return result
+
 class Solution(object):
     def mergeKLists(self, lists):
-        """
-        :type lists: List[ListNode]
-        :rtype: ListNode
-        """
-        
+        result = []
+        count = 0
+        i = 0
+        k = len(lists)
+
+        if k == 0:
+            return lists
+        else:
+            while True:
+                if lists[i] == None:
+                    count = count + 1
+                else:
+                    result.append(lists[i].val)
+                    lists[i] = lists[i].next
+                
+                if count == k:
+                    break
+
+                if i + 1 == k:
+                    i = 0
+                    count = 0
+                else:
+                    i = i + 1
+
+        return sorted(result)
--- 464_can_i_win.py
+++ 464_can_i_win.py
@@ -3,3 +3,4 @@ class Solution(object):
         # Winner is the one who make the total value >= desiredTotal
         # Can the first player force a win
         # eg: canIWin(10, 11) => false
+		
