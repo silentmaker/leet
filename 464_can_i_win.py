bc72ad4962743ac0418302e8999dfcb62a27a267
silentmaker <demonmaxc@gmail.com>
2017-06-02 19:31:07 +0800

    can i win unsolved

--- 464_can_i_win.py
+++ 464_can_i_win.py
@@ -1,6 +1,13 @@
 class Solution(object):
     def canIWin(self, maxChoosableInteger, desiredTotal):
         # Winner is the one who make the total value >= desiredTotal
-        # Can the first player force a win
-        # eg: canIWin(10, 11) => false
-		
+        # Can the first player force a win, eg: canIWin(10, 11) => false
+
+        if maxChoosableInteger == 0:
+            return False
+        elif maxChoosableInteger == 1:
+            return desiredTotal >= 1
+        elif desiredTotal <= maxChoosableInteger or desiredTotal >= maxChoosableInteger + 2:
+            return True
+        else:
+            return False
