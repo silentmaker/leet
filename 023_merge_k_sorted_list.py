aa3614069c5d480f7f9fdbe47805283679aaf6c2
silentmaker <demonmaxc@gmail.com>
2017-06-03 13:52:59 +0800

    merge k sorted list todo

--- 023_merge_k_sorted_list.py
+++ 023_merge_k_sorted_list.py
@@ -4,38 +4,7 @@
 #         self.val = x
 #         self.next = None
 
-# class Solution(object):
-#     def mergeKLists(self, lists):
-#         # lists: List[ListNode]
-#         # example testing: [[1,2,3],[3,3,6]] => [1,2,3,3,3,6]
-#         result = []
-#         minium = 'MAX'
-#         min_index = 'M_INDEX'
-#         count = 0
-#         i = 0
-#         k = len(lists)
-#         if k == 0:
-#             return lists
-#         else:
-#             while True:
-#                 if lists[i] == None:
-#                     count = count + 1
-#                 elif lists[i].val < minium:
-#                     minium = lists[i].val
-#                     min_index = i           
-#                 if count == k:
-#                     break
-#                 if i + 1 == k:
-#                     i = 0
-#                     if min_index != 'M_INDEX':
-#                         lists[min_index] = lists[min_index].next
-#                     result.append(minium)
-#                     minium = 'MAX'
-#                     min_index = 'M_INDEX'
-#                     count = 0
-#                 else:
-#                     i = i + 1
-#         return result
+# TODO: better solution regardless of python itself
 
 class Solution(object):
     def mergeKLists(self, lists):
